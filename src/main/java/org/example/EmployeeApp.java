215a9afe666639c137b93e93731624ad5cbb68a6
Sautov <seeee8212@gmail.com>
2023-08-18 23:35:08 +0300

    Графическое улучшение

--- src/main/java/org/example/EmployeeApp.java
+++ src/main/java/org/example/EmployeeApp.java
@@ -11,50 +11,56 @@ import java.sql.ResultSet;
 
 public class EmployeeApp {
 
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/employee";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+
     private final JFrame frame;
-    private JTextField idField, nameField, ageField, positionField;
-    private JTable table;
-    private DefaultTableModel tableModel;
+    private final JTextField idField;
+    private final JTextField nameField;
+    private final JTextField ageField;
+    private final JTextField positionField;
+    private final JTable table;
+    private final DefaultTableModel tableModel;
 
     public EmployeeApp() {
         frame = new JFrame("Employee Management");
-        JPanel panel = new JPanel(new GridLayout(7, 2));
 
-        panel.add(new JLabel("ID:"));
-        idField = new JTextField(15);
-        panel.add(idField);
+        Font regularFont = new Font("Arial", Font.PLAIN, 15);
+        Font boldFont = new Font("Arial", Font.BOLD, 15);
 
-        panel.add(new JLabel("Name:"));
-        nameField = new JTextField(15);
-        panel.add(nameField);
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0.5;
+        constraints.insets = new Insets(10, 10, 10, 10);
 
-        panel.add(new JLabel("Age:"));
-        ageField = new JTextField(15);
-        panel.add(ageField);
+        addComponent(panel, createLabel("ID:", boldFont), constraints, 0, 0);
+        idField = createTextField(regularFont);
+        addComponent(panel, idField, constraints, 1, 0);
 
-        panel.add(new JLabel("Job_title:"));
-        positionField = new JTextField(15);
-        panel.add(positionField);
+        addComponent(panel, createLabel("Name:", boldFont), constraints, 0, 1);
+        nameField = createTextField(regularFont);
+        addComponent(panel, nameField, constraints, 1, 1);
+
+        addComponent(panel, createLabel("Age:", boldFont), constraints, 0, 2);
+        ageField = createTextField(regularFont);
+        addComponent(panel, ageField, constraints, 1, 2);
+
+        addComponent(panel, createLabel("Job_title:", boldFont), constraints, 0, 3);
+        positionField = createTextField(regularFont);
+        addComponent(panel, positionField, constraints, 1, 3);
 
         JButton addButton = new JButton("Add employee");
+        addButton.setFont(boldFont);
         addButton.addActionListener(e -> {
             addEmployee();
             updateTable();
         });
-        panel.add(addButton);
+        addComponent(panel, addButton, constraints, 1, 4);
 
-        JButton deleteButton = new JButton("Drop");
-        deleteButton.addActionListener(e -> {
-            int selectedRow = table.getSelectedRow();
-            if (selectedRow != -1) {
-                int idToDelete = (int) tableModel.getValueAt(selectedRow, 0);
-                deleteEmployee(idToDelete);
-                updateTable();
-            } else {
-                JOptionPane.showMessageDialog(frame, "Select the line to delete.");
-            }
-        });
-        panel.add(deleteButton);
+        JButton deleteButton = createDeleteButton(boldFont);
+        addComponent(panel, deleteButton, constraints, 1, 5);
 
         tableModel = new DefaultTableModel(new Object[]{"ID", "Name", "Age", "Job_title"}, 0) {
             @Override
@@ -63,6 +69,9 @@ public class EmployeeApp {
             }
         };
         table = new JTable(tableModel);
+        table.setFont(regularFont);
+        table.getTableHeader().setFont(boldFont);
+        table.setRowHeight(25);
         JScrollPane scrollPane = new JScrollPane(table);
 
         frame.setLayout(new BorderLayout());
@@ -85,8 +94,42 @@ public class EmployeeApp {
         });
     }
 
+    private JButton createDeleteButton(Font boldFont) {
+        JButton deleteButton = new JButton("Drop");
+        deleteButton.setFont(boldFont);
+        deleteButton.addActionListener(e -> {
+            int selectedRow = table.getSelectedRow();
+            if (selectedRow != -1) {
+                int idToDelete = (int) tableModel.getValueAt(selectedRow, 0);
+                deleteEmployee(idToDelete);
+                updateTable();
+            } else {
+                JOptionPane.showMessageDialog(frame, "Select the line to delete.");
+            }
+        });
+        return deleteButton;
+    }
+
+    private JLabel createLabel(String text, Font font) {
+        JLabel label = new JLabel(text);
+        label.setFont(font);
+        return label;
+    }
+
+    private JTextField createTextField(Font font) {
+        JTextField textField = new JTextField(15);
+        textField.setFont(font);
+        return textField;
+    }
+
+    private void addComponent(JPanel panel, Component component, GridBagConstraints constraints, int x, int y) {
+        constraints.gridx = x;
+        constraints.gridy = y;
+        panel.add(component, constraints);
+    }
+
     private void addEmployee() {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "root")) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             String sql = "INSERT INTO employee (id, name, age, position) VALUES (?, ?, ?, ?)";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 preparedStatement.setInt(1, Integer.parseInt(idField.getText()));
@@ -114,7 +157,7 @@ public class EmployeeApp {
     private void updateTable() {
         tableModel.setRowCount(0);
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "root")) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             String sql = "SELECT * FROM employee";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 ResultSet resultSet = preparedStatement.executeQuery();
@@ -135,7 +178,7 @@ public class EmployeeApp {
     }
 
     private void deleteEmployee(int id) {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "root")) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             String sql = "DELETE FROM employee WHERE id = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 preparedStatement.setInt(1, id);
@@ -154,7 +197,7 @@ public class EmployeeApp {
     private void updateEmployee(int id, int column, Object value) {
         String columnName = tableModel.getColumnName(column);
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "root")) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             String sql = "UPDATE employee SET " + columnName + " = ? WHERE id = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 if (value instanceof String) {
@@ -162,8 +205,13 @@ public class EmployeeApp {
                 } else if (value instanceof Integer) {
                     preparedStatement.setInt(1, (Integer) value);
                 }
+
                 preparedStatement.setInt(2, id);
-                preparedStatement.executeUpdate();
+
+                int rowsAffected = preparedStatement.executeUpdate();
+                if (rowsAffected == 0) {
+                    JOptionPane.showMessageDialog(frame, "Could not update employee.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -172,6 +220,6 @@ public class EmployeeApp {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(EmployeeApp::new);
+        new EmployeeApp();
     }
 }
